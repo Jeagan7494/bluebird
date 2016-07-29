df7170e73e728385f23fb073eae0175d6f1839a6
p1zzadog <steve.moody@motili.com>
2016-07-29 09:23:44 -0600

    fixed syntax errors in main.js

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,65 +1,47 @@
-angular.module('bluebirdApp', ['ui.router']);
-
-
-angular.module('bluebirdApp')
+angular
+    .module('bluebirdApp', ['ui.router'])
     .config(['$stateProvider',
         function($stateProvider){
-
-            console.log('config loaded');
-
             $stateProvider
                 .state('/main', {
                     url: "",
                     templateUrl: "html/views/work.html"
                 })
-
                 .state('work', {
                     url: "/work",
                     templateUrl: "html/views/work.html"
                 })
-
                 .state('our-story', {
                     url: "/our-story",
                     templateUrl: "html/views/story.html"
                 })
-
                 .state('what-we-do', {
                     url: "/what-we-do",
                     templateUrl: "html/views/what.html"
                 })
-
                 .state('contact', {
                     url: "/contact",
                     templateUrl: "html/views/contact.html"
                 })
-
         }
-    ]);
+    ])
+    .controller('mainController', ['$scope', '$http', function($scope, $http) {
+        console.log('mainController loaded');
 
-    
-angular.module('bluebirdApp').controller('mainController', ['$scope', '$http', function($scope, $http) {
-    console.log('mainController loaded');
+        $scope.form = {};
 
-    $scope.form = {};
-
-    $scope.formSubmit = function() {
-        console.log($scope.form)
-        //$http.post('/submit', $scope.form)
-        $http({
-            method  : 'post',
-            url     : '/api/register',
-            data    : $scope.form
-        })
-            // .then(function(returnData) {
-        //    if (returnData.data.success) {
-        //        window.location.href = "/contactSuccess";
-        //    }
+        $scope.formSubmit = function() {
+            console.log($scope.form)
+            //$http.post('/submit', $scope.form)
+            $http({
+                method  : 'post',
+                url     : '/api/register',
+                data    : $scope.form
+            })
+            .then(function(returnData) {
+               if (returnData.data.success) {
+                   window.location.href = "/contactSuccess";
+               }
+            });
         }
-    };
-
-
-
-
-
-//===================================================================//
-   
+    }]);
